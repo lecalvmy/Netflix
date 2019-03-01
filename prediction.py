38cb379ea37aface0c129df9df784a3ec1c7f73d
Teukeng <42962349+Teukeng@users.noreply.github.com>
2019-03-01 13:35:50 +0100

    algorithme de prédiction

--- prediction.py
+++ prediction.py
@@ -1,13 +1,22 @@
 
 def prediction(U, V, Atest):
-    d = 0
     MAE = 0
     MSE = 0
+    #On sélectionne les couples (user, film) de la matrice A qui ont une note
+    a = np.where(Atest != 0)
+    user = a[0]
+    film = a[1]
     nb_user = U.shape[0]
     for u in range(nb_user):
-        for Aui in Atest:
-            d += 1
-            MAE += abs(Aui - )
-            MSE +=
-    MAE /= (nb_user * d)
+        d = 0
+        indice_u = np.where(user == u)
+        couple_ui = film[indice_u]
+        MAE_u = 0
+        MSE_u = 0
+        for i in couple_ui:
+            d = d+1
+            MAE_u += abs(A[u][i] - np.vdot(U[u, :],V[i, :]))
+            MSE_u += (A[u][i] - np.vdot(U[u, :],V[i, :]))**2
+        MAE += MAE_u/d
+        MSE += MSE_u/d
     return MSE, MAE
